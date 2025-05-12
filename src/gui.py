9a9c5ac54945f03c01628aae2897d01445b44e4d
katsavriasvasilis <143111729+katsavriasvasilis@users.noreply.github.com>
2025-05-12 16:48:37 +0300

    gui.py
    Create GUI skeleton in src/gui.py

--- src/gui.py
+++ src/gui.py
@@ -1,1 +1,17 @@
+import tkinter as tk
+from src.db import init_db
 
+def main():
+    init_db()
+    root = tk.Tk()
+    root.title("Finance App")
+    root.geometry("1024x576")
+
+    # απλό label για δοκιμή
+    lbl = tk.Label(root, text="Welcome to Finance-App", font=("Arial", 24))
+    lbl.pack(pady=20)
+
+    root.mainloop()
+
+if __name__ == "__main__":
+    main()
