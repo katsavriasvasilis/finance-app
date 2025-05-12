7f19537e93a8d0d2d4b2205cd92e37f1cc49f9aa
katsavriasvasilis <143111729+katsavriasvasilis@users.noreply.github.com>
2025-05-12 16:47:28 +0300

    db.py
    Implement src/db.py: sqlite connection & init_db()

--- src/db.py
+++ src/db.py
@@ -1,1 +1,36 @@
+import sqlite3
+from sqlite3 import Connection
+
+def get_conn(db_path: str = "data/finance.db") -> Connection:
+    conn = sqlite3.connect(db_path)
+    conn.row_factory = sqlite3.Row
+    return conn
+
+def init_db(db_path: str = "data/finance.db"):
+    sql_create_categories = """
+    CREATE TABLE IF NOT EXISTS categories (
+        id INTEGER PRIMARY KEY,
+        name TEXT NOT NULL,
+        type TEXT CHECK(type IN ('income','expense')) NOT NULL,
+        is_monthly INTEGER NOT NULL DEFAULT 0
+    );"""
+    sql_create_transactions = """
+    CREATE TABLE IF NOT EXISTS transactions (
+        id INTEGER PRIMARY KEY,
+        category_id INTEGER NOT NULL,
+        amount REAL NOT NULL,
+        date TEXT NOT NULL,
+        description TEXT,
+        FOREIGN KEY(category_id) REFERENCES categories(id)
+    );"""
+    conn = get_conn(db_path)
+    with conn:
+        conn.execute(sql_create_categories)
+        conn.execute(sql_create_transactions)
+    conn.close()
+
+if __name__ == "__main__":
+    init_db()
+    print("Initialized database at", "data/finance.db")
+
 
