f5451c7aa5c41c397281d51265c9e214a08c196b
katsavriasvasilis <143111729+katsavriasvasilis@users.noreply.github.com>
2025-05-19 15:48:55 +0300

    Προσθήκη view.py και controller.py stubs
    Skeleton GUI με 3 καρτέλες και αρχική κλάση AppController με binding framework

--- src/view.py
+++ src/view.py
@@ -1,1 +1,21 @@
+"""
+GUI components με tkinter.
+"""
+import tkinter as tk
+from tkinter import ttk
+
+class MainWindow(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        self.title("Personal Finance Manager")
+        self.geometry("800x600")
+        tab_control = ttk.Notebook(self)
+        self.tab_categories = ttk.Frame(tab_control)
+        self.tab_transactions = ttk.Frame(tab_control)
+        self.tab_reports = ttk.Frame(tab_control)
+        tab_control.add(self.tab_categories, text='Κατηγορίες')
+        tab_control.add(self.tab_transactions, text='Συναλλαγές')
+        tab_control.add(self.tab_reports, text='Αναφορές')
+        tab_control.pack(expand=1, fill='both')
+        # TODO: Προσθήκη widgets σε κάθε tab
 
