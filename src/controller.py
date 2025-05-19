b1d388761a03ba90efff2aca895f8a2fe5c325d6
katsavriasvasilis <143111729+katsavriasvasilis@users.noreply.github.com>
2025-05-19 15:50:35 +0300

    Προσθήκη view.py και controller.py stubs
    Skeleton GUI με 3 καρτέλες και αρχική κλάση AppController με binding framework

--- src/controller.py
+++ src/controller.py
@@ -1,1 +1,23 @@
+"""
+Λογική εφαρμογής: σύνδεση Model και View.
+"""
+from model import Database
+from view import MainWindow
+
+class AppController:
+    def __init__(self):
+        self.db = Database()
+        self.ui = MainWindow()
+        self._bind_events()
+
+    def _bind_events(self):
+        # TODO: Όταν φορτώνει κάθε tab, γεμίζουμε Widgets από τη ΒΔ
+        pass
+
+    def run(self):
+        self.ui.mainloop()
+
+if __name__ == "__main__":
+    app = AppController()
+    app.run()
 
